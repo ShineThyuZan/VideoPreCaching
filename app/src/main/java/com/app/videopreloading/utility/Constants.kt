9b4fa066254e2aa36b5d83eff971e3d0d7432a57
Shine Thyu Zan <shinethyuzan2010@gmail.com>
2024-01-15 15:32:13 +0630

    constant file modification

--- app/src/main/java/com/app/videopreloading/utility/Constants.kt
+++ app/src/main/java/com/app/videopreloading/utility/Constants.kt
@@ -3,6 +3,4 @@ package com.app.videopreloading.utility
 object Constants {
     const val VIDEO_LIST = "VIDEO_LIST"
     const val VIDEO_URL = "VIDEO_URL"
-    const val STUDIO = "Studio"
-    const val PUSSY_CAT = "CatCat"
 }

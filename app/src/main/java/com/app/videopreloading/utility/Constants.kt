57f16d236d6bf9f011b7e0dd5f8c9bbb7c834317
Shine Thyu Zan <shinethyuzan2010@gmail.com>
2024-01-15 13:10:16 +0630

    const file delete

--- app/src/main/java/com/app/videopreloading/utility/Constants.kt
+++ app/src/main/java/com/app/videopreloading/utility/Constants.kt
@@ -3,6 +3,4 @@ package com.app.videopreloading.utility
 object Constants {
     const val VIDEO_LIST = "VIDEO_LIST"
     const val VIDEO_URL = "VIDEO_URL"
-    const val VIDEO_PRE_LOADING_SERVICE = "VideoPreLoadingService"
-    const val VIDEO_PRE_SERVICE = "VideoPreService"
 }

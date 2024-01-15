47dfe99d0d211bb15afa585bdf4294fd0def4610
Shine Thyu Zan <shinethyuzan2010@gmail.com>
2024-01-15 13:04:56 +0630

    Constant file upload

--- app/src/main/java/com/app/videopreloading/utility/Constants.kt
+++ app/src/main/java/com/app/videopreloading/utility/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val VIDEO_LIST = "VIDEO_LIST"
     const val VIDEO_URL = "VIDEO_URL"
     const val VIDEO_PRE_LOADING_SERVICE = "VideoPreLoadingService"
+    const val VIDEO_PRE_SERVICE = "VideoPreService"
 }
